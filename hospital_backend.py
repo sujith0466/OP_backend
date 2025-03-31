2ca3b7d313f036bf55eceeacce5bf5f6f44d0b0a
Sujith Kumar Sanisetty <sujithsunil4818@gmail.com>
2025-03-31 17:44:52 +0530

    Update hospital_backend.py

--- hospital_backend.py
+++ hospital_backend.py
@@ -40,4 +40,4 @@ def generate():
         return jsonify({"error": "Internal Server Error"}), 500
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
